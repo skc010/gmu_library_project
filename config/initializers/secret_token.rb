3387b3020d5bd1f33ab032a80d41ff5297b5c60f
Sean_Cheetham <skc010@bucknell.edu>
2014-10-09 18:57:46 -0400

    Initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GmuLibraryProject::Application.config.secret_key_base = '6fb29fc6960739d377a81be2c07a54615fdd39e7c642fc64f6ef3bf5d546f2beb5a200386716b4737686fc26d310214ddaf962a2d57d2dd7d0e2e4180c8bb561'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GmuLibraryProject::Application.config.session_store :cookie_store, key: '_gmu_library_project_session'

4389959e960df0defd3c20d450b43b4f2b46d461
mdjoum <mdjoum@gmail.com>
2014-10-09 21:23:22 -0400

    book seed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,12 @@
 class Book < ActiveRecord::Base
 	validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, :author, presence: true 
-	validates :genre, inclusion:GENRE 
-	validates :author. inclusion:AUTHOR
+	validates :genre, inclusion:{in: %w(science fiction, fiction, non-fiction, fantasy, romance, classics)}
 	validates :abstract, length: {minimum: 15} 
-	validates :pages, :total_in_library
+	validates :pages,
 	  numericality: {only_integer: true, greater_than_or_equal_to:0},
+	  unless: "pages.blank?"
+	validates :total_in_library,
+	  numericality: {greater_than_or_equal_to: 0},
 	  unless: "total_in_library.blank?"
 
 	has_one :author
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Author.create(name:'J. R. R. Tolkien', dob:Time.parse('03-01-1892'), nationality: 'British', biography:'John Ronald Reuel Tolkien, CBE was an English writer, poet, philologist, and university professor, best known as the author of the classic high fantasy works The Hobbit, The Lord of the Rings, and The Silmarillion.', image_url: 'http://en.wikipedia.org/wiki/J._R._R._Tolkien#mediaviewer/File:Tolkien_1916.jpg')
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+tolkien = Author.where(name:'J. R. R. Tolkien')
+                .first_or_create!(dob:Time.parse('03-01-1892'), nationality:'British', 
+                	biography:"John Ronald Reuel Tolkien, CBE was an English writer, poet, 
+                	philologist, and university professor, best known as the author of the 
+                	classic high fantasy works The Hobbit, The Lord of the Rings, and The 
+                	Silmarillion.", image_url: 'http://en.wikipedia.org/wiki/J._R._R._Tolkien#mediaviewer/File:Tolkien_1916.jpg')
+Book.where(title:'The Lord of the Rings')
+	.first_or_create!(isbn:'9780544003415', abstract:'This is where the short summary for lord of the rings goes', 
+	pages:1216, genre:'fantasy', published_on:07/29/1954, total_in_library:2, author:tolkien)
+

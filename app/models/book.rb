3ae54d46e86f961e86cefd95513bfe4129b3da18
Vikram Hakkal <vhakkal@gmail.com>
2014-10-16 19:21:31 -0400

    nothing major

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,14 @@
 class Book < ActiveRecord::Base
+
 	validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, :author, presence: true 
 	validates :genre, inclusion:{in: %w(science-fiction fiction non-fiction fantasy romance classics)}
 	validates :abstract, length: {minimum: 15} 
 	validates :pages,
 	  numericality: {only_integer: true, greater_than_or_equal_to:0},
 	  unless: "pages.blank?"
+
+	 validates :author, inclusion: {in: AUTHORS, message: "must be from #{RATINGS.first} to #{RATINGS.last}" }
+
 	validates :total_in_library,
 	  numericality: {greater_than_or_equal_to: 0},
 	  unless: "total_in_library.blank?"

343e9d4e0f0283529c1b60c0f57e9ce0589a7a2d
Vikram Hakkal <vhakkal@gmail.com>
2014-10-16 19:42:54 -0400

    updates for the create/update book page

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,16 +1,13 @@
 class Book < ActiveRecord::Base
-
+	GENRES = ['science-fiction', 'fiction', 'non-fiction', 'fantasy', 'romance', 'classics']
 	validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, :author, presence: true 
-	validates :genre, inclusion:{in: %w(science-fiction fiction non-fiction fantasy romance classics)}
+	validates :genre, inclusion: { in: GENRES}
 	validates :abstract, length: {minimum: 15} 
 	validates :pages,
-	  numericality: {only_integer: true, greater_than_or_equal_to:0},
+	  numericality: {only_integer: true, greater_than_or_equal_to: 0},
 	  unless: "pages.blank?"
-
-	 validates :author, inclusion: {in: AUTHORS, message: "must be from #{RATINGS.first} to #{RATINGS.last}" }
-
 	validates :total_in_library,
-	  numericality: {greater_than_or_equal_to: 0},
+	  numericality: {nly_integer: true, greater_than_or_equal_to: 0},
 	  unless: "total_in_library.blank?"
 
 	belongs_to :author
